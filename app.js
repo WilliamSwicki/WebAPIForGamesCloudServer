9161e7efdbaee6fba2f448436d5fb1d7db5733c4
WilliamSwicki <wmswicki@email.neit.edu>
2025-02-19 17:58:59 -0500

    merged missing branch 4 files

--- app.js
+++ app.js
@@ -11,6 +11,20 @@ const { register } = require("module");
 const app = express();
 const port = process.env.port || 3000;
 
+const itemSchema = new mongoose.Schema({
+    item:String
+});
+
+const Item = mongoose.model("itemdata", itemSchema, "itemdata");
+
+const userSchema = new mongoose.Schema({
+    username:{type:String, required:true, unique:true},
+    password:{type:String, required:true},
+    email:{type:String, required:true}
+});
+
+const User = mongoose.model("User", userSchema, "users");
+
  //middleware to serve static data
  app.use(express.static(path.join(__dirname, "public")));
 
@@ -75,9 +89,58 @@ app.post("/register", async (req,res)=>{
      responce.sendFile(path.join(__dirname,"public","index.html"));
  });
 
- app.get("/addEntry",function(request, responce){
+ app.get("/logedIndex",isAuthenticated,(req,res)=>{
+    res.sendFile(path.join(__dirname,"public","logedIndex.html"));
+});
+
+app.get("/update/:id", isAuthenticated, async function(request, responce){
+    try{
+        const itemId = request.params.id;
+
+        const filePath = path.join(__dirname, 'public', 'update.html');
+        console.log(filePath);
+        responce.sendFile(filePath,{
+            headers:{
+                'Cache-Control':'no-store'
+            }
+        });
+    } catch(err){
+        responce.status(500).json({error:"server error"});
+    }
+});
+
+app.post("/updateItem/:id", async (req, res)=>{
+    const itemId = req.params.id;
+    const updatedItem = req.body.item; 
+
+    try{
+        const newItem = await Item.findByIdAndUpdate(itemId,{item:updatedItem},{new: true});
+        if (!newItem) {
+            return res.status(404).send("Item not found"); 
+        }
+        res.redirect('/logedIndex.html');
+    }catch(err){
+        console.error("Error updating item:", error);
+        res.status(500).send("Server error while updating item");
+    }
+    
+});
+
+ app.get("/addEntry",isAuthenticated,function(request, responce){
     // responce.send("Hello Everyone!");
     responce.sendFile(path.join(__dirname,"public","addEntry.html"));
+});
+
+app.post("/addEntry", async(req,res)=>{
+    try{
+        const newItem = new Item(req.body);
+        const saveItem = await newItem.save();
+
+        res.redirect("/logedIndex");
+        console.log(saveItem);
+    }catch(err){
+        res.status(501).json({error:"Failed to add new person."});
+    }
 });
 
 app.get("/login",function(request, responce){
@@ -85,8 +148,62 @@ app.get("/login",function(request, responce){
     responce.sendFile(path.join(__dirname,"public","login.html"));
 });
 
- app.get("/testjson",(req, res)=>{
-     res.sendFile(path.join(__dirname,"public","json/games.json"));
+app.post("/login", async (req,res)=>{
+    const {username, password} = req.body;
+    console.log(req.body);
+
+    const user = await User.findOne({username});
+    
+    if(user && bcrypt.compareSync(password, user.password)){
+       
+        req.session.user = username;
+        return res.redirect("/logedIndex");
+    }
+    req.session.error = "Invalid User";
+    return res.redirect("/login")
+});
+
+app.get("/logout", (req,res)=>{
+    req.session.destroy(()=>{
+        res.redirect("/login");
+    })
+});
+
+app.delete("/deleteitem/:id",isAuthenticated, async (req,res)=>{
+    try{
+        const item = await Item.findById(req.params.id);
+        
+        console.log(item);
+        if(item.length==0){
+            return res.status(404).json({error:"Failed to find the person."});
+        }
+        const deletedItem = await Item.findByIdAndDelete(item);
+        
+        res.sendFile(path.join(__dirname,"public","index.html"));
+    }catch(err){
+        res.status(404).json({error:"item not found"});
+    }
+});
+
+ app.get("/testjson", async (req, res)=>{
+    try{
+        const items = await Item.find();
+        res.json(items);
+        console.log(items);
+    }catch(err){
+        res.status(500).json({error:"Failed to get items"});
+    }
+    //  res.sendFile(path.join(__dirname,"public","json/games.json"));
+ });
+
+ app.get("/testjson/:id", async (req, res)=>{
+    try{
+        const items = await Item.findById(req.params.id);
+        res.json(items);
+    }catch(err){
+        res.status(500).json({error:"Failed to get items"});
+    }
+    //  res.sendFile(path.join(__dirname,"public","json/games.json"));
  });
 
 setTimeout(()=>{
